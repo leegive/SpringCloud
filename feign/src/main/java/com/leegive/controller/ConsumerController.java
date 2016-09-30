3150967e94d6724b0b74299a83e3665445ad74d9
leegive <leegive@icloud.com>
2016-10-01 07:45:40 +0800

    test config

--- feign/src/main/java/com/leegive/controller/ConsumerController.java
+++ feign/src/main/java/com/leegive/controller/ConsumerController.java
@@ -2,6 +2,8 @@ package com.leegive.controller;
 
 import com.leegive.client.ComputeClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by leegive on 30/09/2016.
  */
+@RefreshScope
 @RestController
 public class ConsumerController {
 
     @Autowired
     ComputeClient computeClient;
 
+    @Value("${from}")
+    private String from;
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add() {
         return computeClient.add(10, 20);
+    }
+
+    @RequestMapping(value = "/from", method = RequestMethod.GET)
+    public String from(){
+        return from;
     }
 
 }
