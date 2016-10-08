a87efcac7aa22b7a3f86ea7cd9271e325b4784b3
leegive <leegive@icloud.com>
2016-10-08 15:18:26 +0800

    dfdf

--- zuul/src/main/java/com/leegive/ZuulApplication.java
+++ zuul/src/main/java/com/leegive/ZuulApplication.java
@@ -1,19 +1,14 @@
 package com.leegive;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @SpringCloudApplication
+@EnableOAuth2Sso
 @EnableZuulProxy
 public class ZuulApplication {
-
-	@Bean
-	public AccessFilter accessFilter() {
-		return new AccessFilter();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
